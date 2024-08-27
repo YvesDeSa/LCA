4d35b647b93366f484c75fc205efaf69ebe14947
Yves de Sá <yvesdsb98@gmail.com>
2024-08-27 15:43:24 -0300

    ✅ Adicinando as rotas uptade e delete de co-produtos

--- src/routes/coProductRoutes.ts
+++ src/routes/coProductRoutes.ts
@@ -6,5 +6,7 @@ const router = Router();
 router.post('/coProduct', CoProductController.add);
 router.get('/coProduct', CoProductController.getAll);
 router.get('/coProduct/:id', CoProductController.getById);
+router.put('/coProduct/:id/edit', CoProductController.update);
+router.post('/coProduct/:id/delete', CoProductController.delete);
 
 export default router;
