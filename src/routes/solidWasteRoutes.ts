2870ef89d154265f11eae1dd4c4f015bf473a4f4
Yves de Sá <yvesdsb98@gmail.com>
2024-08-27 15:44:08 -0300

    ✅ Adicionando as rotas update e delete de resídus solídos

--- src/routes/solidWasteRoutes.ts
+++ src/routes/solidWasteRoutes.ts
@@ -6,5 +6,7 @@ const router = Router();
 router.post('/solidWaste', SolidWasteController.add);
 router.get('/solidWaste', SolidWasteController.getAll);
 router.get('/solidWaste/:id', SolidWasteController.getById);
+router.put('/solidWaste/:id/edit', SolidWasteController.update);
+router.post('/solidWaste/:id/delete', SolidWasteController.delete);
 
 export default router;
